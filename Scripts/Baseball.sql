a945b8012f15f26abbaf3e200a871867e671c22a
TomMaster <hercules_masteriv@hotmail.com>
2023-10-28 13:57:42 -0500

    Baseball 20231028C

--- Scripts/Baseball.sql
+++ Scripts/Baseball.sql
@@ -321,12 +321,52 @@ SELECT
  CAST(COUNT(DISTINCT(YEARID))AS NUMERIC))*100,2) PERCENTAGE
 FROM X
 
--- 12 times out of 46 season
+-- 12 times out of 46 seasons
 -- 26.09
 
 -- 8. Using the attendance figures from the homegames table, find the teams and parks which had the top 5 average attendance per game in 2016 (where average attendance is defined as total attendance divided by number of games). Only consider parks where there were at least 10 games played. Report the park name, team name, and average attendance. Repeat for the lowest 5 average attendance.
 
 
+WITH X AS
+(SELECT
+ 'HIGHEST TOP 5 ATTENDANCE' ATTENANCE_TYPE,
+ UPPER(T.NAME) TEAM_NAME,
+ UPPER(T.PARK)BAll_PARK,
+ H.ATTENDANCE/H.GAMES AVG_ATTENDANCE,
+ H.ATTENDANCE,
+ H.GAMES,
+ RANK() OVER (ORDER BY  H.ATTENDANCE/H.GAMES DESC) RANKING
+FROM
+ HOMEGAMES H
+ INNER JOIN TEAMS T ON H.TEAM = T.TEAMID AND H.YEAR = T.YEARID
+WHERE
+ H.GAMES >=10 AND
+ H.YEAR = 2016
+UNION
+SELECT
+ 'LOWEST TOP 5 ATTENDANCE' ATTENANCE_TYPE,
+ UPPER(T.NAME) TEAM_NAME,
+ UPPER(T.PARK)BAll_PARK,
+ H.ATTENDANCE/H.GAMES AVG_ATTENDANCE,
+ H.ATTENDANCE,
+ H.GAMES,
+ RANK() OVER (ORDER BY  H.ATTENDANCE/H.GAMES ASC) RANKING
+FROM
+ HOMEGAMES H
+ INNER JOIN TEAMS T ON H.TEAM = T.TEAMID AND H.YEAR = T.YEARID
+WHERE
+ H.GAMES >=10 AND
+ H.YEAR = 2016)
+SELECT
+ RANKING,
+ ATTENANCE_TYPE
+ TEAM_NAME,
+ BALL_PARK,
+ AVG_ATTENDANCE
+FROM X 
+WHERE RANKING BETWEEN 1 AND 5
+ORDER BY
+2 DESC,1
 
 
 -- 9. Which managers have won the TSN Manager of the Year award in both the National League (NL) and the American League (AL)? Give their full name and the teams that they were managing when they won the award.
